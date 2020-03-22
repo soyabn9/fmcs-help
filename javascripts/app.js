f411145dfc7e681ba3c4bd30ed90be47668831e1
soyabn9 <mustaqnandhla@gmail.com>
2020-03-22 15:44:50 +0000

    Deployed 69071f1 with MkDocs version: 1.0.4

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,11 +3,3 @@ function Reload() {
 }
 
 document.getElementById("Refresh").addEventListener("click", Reload);
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js").then(reg => {
-            console.log("Service worker registered.", reg);
-        });
-    });
-}
